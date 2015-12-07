fb8b0b287fd642dd9bf6bca1e9ca152bae76dae0
Jiayi <kuangtoh@gmail.com>
2015-12-07 23:55:37 +0800

    Initial Commit

---
+++ StringHelper/StringHelper.h
@@ -0,0 +1,19 @@
+//
+//  StringHelper.h
+//  StringHelper
+//
+//  Created by Hong Wei Zhuo on 7/12/15.
+//  Copyright © 2015 ___zhuohongwei___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringHelper.
+FOUNDATION_EXPORT double StringHelperVersionNumber;
+
+//! Project version string for StringHelper.
+FOUNDATION_EXPORT const unsigned char StringHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringHelper/PublicHeader.h>
+
+
